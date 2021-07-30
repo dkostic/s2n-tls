3e5ad22e38f75281d81d2db63404fe5aaa4c2f8d
Dusan Kostic <dkostic@amazon.com>
2021-07-30 10:53:32 +0200

    testing
    Signed-off-by: Dusan Kostic <dkostic@amazon.com>

--- codebuild/bin/s2n_setup_env.sh
+++ codebuild/bin/s2n_setup_env.sh
@@ -124,7 +124,11 @@ if [[ "$S2N_LIBCRYPTO" == "openssl-1.0.2-fips" ]]; then
 fi
 if [[ "$S2N_LIBCRYPTO" == "boringssl" ]]; then export LIBCRYPTO_ROOT=$BORINGSSL_INSTALL_DIR ; fi
 
-if [[ "$S2N_LIBCRYPTO" == "awslc" ]]; then export LIBCRYPTO_ROOT=$AWSLC_INSTALL_DIR ; fi
+if [[ "$S2N_LIBCRYPTO" == "awslc" ]]; then
+    export LIBCRYPTO_ROOT=$AWSLC_INSTALL_DIR ;
+    export S2N_TEST_IN_FIPS_MODE=1 ;
+    export OPENSSL_FIPS=1 ;
+fi
 
 if [[ "$S2N_LIBCRYPTO" == "libressl" ]]; then export LIBCRYPTO_ROOT=$LIBRESSL_INSTALL_DIR ; fi
 
